a17bc17d53ba9ddf80dea87daccf60a7050c7a35
biratdatta <birat.datta26@gmail.com>
2022-08-06 13:10:43 +0530

    changes

---
+++ weatherapp/node_modules/rollup/dist/es/rollup.js
@@ -0,0 +1,16 @@
+/*
+  @license
+	Rollup.js v2.77.1
+	Tue, 26 Jul 2022 08:02:21 GMT - commit 3be1609e95cf81046bb325b3e3c1cb23a7e972b7
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+export { version as VERSION, defineConfig, rollup, watch } from './shared/rollup.js';
+import 'path';
+import 'process';
+import 'perf_hooks';
+import 'crypto';
+import 'fs';
+import 'events';
