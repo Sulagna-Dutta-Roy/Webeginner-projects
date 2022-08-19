f1f75835e215901140ccf30e07ec845c947a0baf
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2022-08-20 03:43:25 +0530

    Added functionality to customize length of Password

--- Random Password Generator/main.js
+++ Random Password Generator/main.js
@@ -1,11 +1,33 @@
 var password = document.getElementById("password");
+
+
+// var options = document.querySelector('.options');
+//
+// options.querySelector('#min').onchange = function () {
+//     generator.setMin(this.value);
+// };
+
+// console.log(textoc);
 // @dev The function getPassword() is used to generate a random password.
 function getPassword() {
+  var numLen = document.getElementById("min");
+  var passwordLength = parseInt(numLen.value);
+
   var chars =
     "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
+    if(passwordLength<=0){
+      passwordLength = 1;
+      numLen.value = "1";
+    }
+    if(passwordLength>15){
+      passwordLength = 15;
+      numLen.value = "15";
+    }
+
+  console.log(passwordLength);
+  console.log(9);
   var password = "";
-  for (var i = 0; i <= passwordLength; i++) {
+  for (var i = 0; i < passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber + 1);
   }
