5ee1ed39245b03bc444e22b2294aae8968d73990
Sanskar Suri <99363431+Sanskar50@users.noreply.github.com>
2022-09-24 12:13:45 +0530

    Modified JS
    Tried to resolve workflow issue

--- Number Guessing Game/index.js
+++ Number Guessing Game/index.js
@@ -17,12 +17,8 @@ function check(){
     if(random == guess)
     {
         ans.innerHTML ="Your Guess is Correct "+random+" is the number";
-<<<<<<< HEAD
-        ans.style.color= "#00FFAB";
-=======
         ans.style.color= "#006400";
         t++;
->>>>>>> 92d675b532b475346458fb0184de69a890533cac
     }
     else if(random>guess && random>1)
     {
